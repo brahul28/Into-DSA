3ed891ce45f9153abc717cb5b16cd8b9e5a436bc
Rahul Bisht <bishtsixstring28@.com>
2026-02-27 11:38:46 +0530

    Add largest number in Array program

---
+++ largestNumber.js
@@ -0,0 +1,12 @@
+function largestNumber(arr){
+    let largest = -Infinity;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    console.log(largest);
+}
+
+let arr = [2, -6, 4, 8, 1, -9];
+largestNumber(arr);
