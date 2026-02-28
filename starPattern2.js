7b1ceb96b981437629d5a3c2883308814c122b9b
Rahul Bisht <bishtsixstring28@.com>
2026-02-28 12:10:28 +0530

    Add second StarPattern program

---
+++ starPattern2.js
@@ -0,0 +1,14 @@
+function starPattern(num) {
+
+    for (let i = 0; i < num; i++) {
+        let row = " ";
+
+        for (let j = 0; j < i + 1; j++) {
+            row = row + "*";
+        }
+        console.log(row);
+    }
+}
+
+let n = 4;
+starPattern(n)
