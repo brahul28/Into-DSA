4ac2fea28579ffc4d399f9b9cf49626cb969fbbb
Rahul Bisht <bishtsixstring28@.com>
2026-02-28 11:52:59 +0530

    Add star paater 4x4

---
+++ starPattern1.js
@@ -0,0 +1,13 @@
+function starPattern(num){
+
+    for(let i = 0; i < num; i++){
+        let row = " ";
+
+        for(let j = 0; j < num; j++){
+            row = row + "*";
+        }
+    }
+}
+
+let n = 4;
+starPattern(n)
