96c1e93eb34b8aef49d58f7f3bc099037206c32e
Rahul Bisht <bishtsixstring28@.com>
2026-02-28 12:18:19 +0530

    Add another condition for second j loop

--- starPattern2.js
+++ starPattern2.js
@@ -3,6 +3,10 @@ function starPattern(num) {
     for (let i = 0; i < num; i++) {
         let row = " ";
 
+        // for (let j = 0; j <= i; j++) {
+        //     row = row + "*";
+        // }
+
         for (let j = 0; j < i + 1; j++) {
             row = row + "*";
         }
