f0ec0965324d5b4b68b6f422ec6355cd939d229a
Rahul Bisht <bishtsixstring28@.com>
2026-03-01 12:34:54 +0530

    Add star Pattern6 program

---
+++ starPattern6.js
@@ -0,0 +1,14 @@
+function starPattern(num) {
+
+    for (let i = 0; i < num; i++) {
+        let row = " ";
+
+        for (let j = 0; j < num -i; j++) {
+            row = row + "*";
+        }
+        console.log(row);
+    }
+}
+
+let n = 5;
+starPattern(n)
