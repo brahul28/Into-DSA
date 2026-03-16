2b73f623cd9b97cae5ca03628c63537c55c3b7a1
Rahul Bisht <bishtsixstring28@.com>
2026-03-16 12:42:10 +0530

    Add move zero program and modify buy and sell stock program

--- buyAndsellStock.js
+++ buyAndsellStock.js
@@ -25,3 +25,22 @@ var maxProfit = function(prices){
 let prices = [7, 1, 5, 3, 6, 4];
 let result = maxProfit(prices);
 console.log(result);
+
+
+
+
+//var maxProfit = function(prices){
+//     let maxProfit = 0;
+//     for(let i =0; i< prices.length; i++){
+//         for(let j = i + 1; j< prices.length; j++){
+//             if(prices[j] - prices[i] > maxProfit){
+//                 maxProfit = prices[j] - prices[i];
+//             }
+//         }
+//     }
+//     return maxProfit;
+// };
+
+// let prices = [7, 1, 5, 3, 6, 4];
+// let result = maxProfit(prices);
+// console.log(result);
