c10fcd43e50393fbc73bb100830754f7d1293eee
Rahul Bisht <bishtsixstring28@.com>
2026-04-14 11:28:26 +0530

    Made minor changes on linear search program

--- linearSearch.js
+++ linearSearch.js
@@ -1,17 +1,16 @@
-
 //Search a number in an array and return the index value
 // if the number is not present in an array return -1
 
+let arr = [4, 5, 1, 3, 9];
 
 function linearSearch(arr, target) {
-
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] === target) {
-            return i;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == target) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
-let arr = [4, 9, 1, 0, 2];
-console.log(linearSearch(arr, 20));
+let result = linearSearch(arr, 5);
+console.log("Element found at index", result);
